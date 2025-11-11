f1fce30414dfc7005cbf5da79ec595537551ef57
tahsinsakib5 <sakibulislam5162@gmail.com>
2025-11-11 13:23:59 +0600

    solve main activity file

--- lib/menu_selection_activity.dart
+++ lib/menu_selection_activity.dart
@@ -15,13 +15,13 @@ class _MenuSelectionActivityState extends State<MenuSelectionActivity> {
   String _selectedMenu = 'Select a Menu';
 
   ExerciseConfig _config = ExerciseConfig(
-  counts: ExerciseCounts(
-    scissor: 0,
-    pencil: 0, 
-    pincher: 0,
-    button: 0,
-  )
-);
+    counts: ExerciseCounts(
+      scissor: 0,
+      pencil: 0, 
+      pincher: 0,
+      button: 0,
+    )
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +39,12 @@ class _MenuSelectionActivityState extends State<MenuSelectionActivity> {
               children: [
                 _menuButton('Menu 0', 'Buttons only activated', () => _setMenu0()),
                 _menuButton('Menu 1', 'Pincher only activated', () => _setMenu1()),
-                // _menuButton('Menu 2', 'Pencils only activated', () => _setMenu2()),
-                // _menuButton('Menu 3', 'Scissors only activated', () => _setMenu3()),
-                // _menuButton('Menu 4', 'Led Only', () => _setMenu4()),
-                // _menuButton('Menu 5', 'No Led', () => _setMenu5()),
-                // _menuButton('Menu 6', 'Repeat 3x', () => _setMenu6()),
-                // _menuButton('Menu 7', 'Repeat 5x', () => _setMenu7()),
+                _menuButton('Menu 2', 'Pencils only activated', () => _setMenu2()),
+                _menuButton('Menu 3', 'Scissors only activated', () => _setMenu3()),
+                _menuButton('Menu 4', 'Led Only', () => _setMenu4()),
+                _menuButton('Menu 5', 'No Led', () => _setMenu5()),
+                _menuButton('Menu 6', 'Repeat 3x', () => _setMenu6()),
+                _menuButton('Menu 7', 'Repeat 5x', () => _setMenu7()),
               ],
             ),
           ),
@@ -103,41 +103,89 @@ class _MenuSelectionActivityState extends State<MenuSelectionActivity> {
     });
   }
 
-  // Add similar methods for menu2-menu7...
+  void _setMenu2() {
+    setState(() {
+      _selectedMenu = 'Selected Menu 2';
+      _config = ExerciseConfig(
+        chkButton: false, chkPencil: true, chkPincher: false, chkScissor: false,
+        musicButton: true, musicPencil: true, musicPincher: true, musicScissor: true,
+        noLed: false, repeatMode: false, mode: 2, modeName: 'Pencils only activated',
+        counts: ExerciseCounts(button: 0, pencil: 0, pincher: 0, scissor: 0),
+      );
+    });
+  }
+
+  void _setMenu3() {
+    setState(() {
+      _selectedMenu = 'Selected Menu 3';
+      _config = ExerciseConfig(
+        chkButton: false, chkPencil: false, chkPincher: false, chkScissor: true,
+        musicButton: true, musicPencil: true, musicPincher: true, musicScissor: true,
+        noLed: false, repeatMode: false, mode: 3, modeName: 'Scissors only activated',
+        counts: ExerciseCounts(button: 0, pencil: 0, pincher: 0, scissor: 0),
+      );
+    });
+  }
+
+  void _setMenu4() {
+    setState(() {
+      _selectedMenu = 'Selected Menu 4';
+      _config = ExerciseConfig(
+        chkButton: true, chkPencil: true, chkPincher: true, chkScissor: true,
+        musicButton: false, musicPencil: false, musicPincher: false, musicScissor: false,
+        noLed: false, repeatMode: false, mode: 4, modeName: 'Led Only',
+        counts: ExerciseCounts(button: 0, pencil: 0, pincher: 0, scissor: 0),
+      );
+    });
+  }
+
+  void _setMenu5() {
+    setState(() {
+      _selectedMenu = 'Selected Menu 5';
+      _config = ExerciseConfig(
+        chkButton: true, chkPencil: true, chkPincher: true, chkScissor: true,
+        musicButton: true, musicPencil: true, musicPincher: true, musicScissor: true,
+        noLed: true, repeatMode: false, mode: 5, modeName: 'No Led',
+        counts: ExerciseCounts(button: 0, pencil: 0, pincher: 0, scissor: 0),
+      );
+    });
+  }
+
+  void _setMenu6() {
+    setState(() {
+      _selectedMenu = 'Selected Menu 6';
+      _config = ExerciseConfig(
+        chkButton: true, chkPencil: true, chkPincher: true, chkScissor: true,
+        musicButton: false, musicPencil: false, musicPincher: false, musicScissor: false,
+        noLed: false, repeatMode: true, mode: 6, modeName: 'Repeat 3x',
+        counts: ExerciseCounts(button: 3, pencil: 3, pincher: 3, scissor: 3),
+      );
+    });
+  }
+
+  void _setMenu7() {
+    setState(() {
+      _selectedMenu = 'Selected Menu 7';
+      _config = ExerciseConfig(
+        chkButton: true, chkPencil: true, chkPincher: true, chkScissor: true,
+        musicButton: false, musicPencil: false, musicPincher: false, musicScissor: false,
+        noLed: false, repeatMode: true, mode: 7, modeName: 'Repeat 5x',
+        counts: ExerciseCounts(button: 5, pencil: 5, pincher: 5, scissor: 5),
+      );
+    });
+  }
 
   void _goToExercise() {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => MainActivity(
-          device: widget.device, config: _config,
-          
+          device: widget.device, 
+          config: _config,
         ),
       ),
     );
   }
 }
 
-// class ExerciseConfig {
-//   final bool chkScissor, chkPencil, chkPincher, chkButton;
-//   final bool musicScissor, musicPencil, musicPincher, musicButton;
-//   final bool noLed, repeatMode;
-//   final int mode;
-//   final String modeName;
-//   final ExerciseCounts counts;
-
-//   ExerciseConfig({
-//     this.chkScissor = false, this.chkPencil = false, this.chkPincher = false, this.chkButton = false,
-//     this.musicScissor = true, this.musicPencil = true, this.musicPincher = true, this.musicButton = true,
-//     this.noLed = false, this.repeatMode = false, this.mode = 0, this.modeName = '',
-//     required this.counts,
-//   });
-// }
-
-// class ExerciseCounts {
-//   final int scissor, pencil, pincher, button;
-
-//   ExerciseCounts({
-//     this.scissor = 0, this.pencil = 0, this.pincher = 0, this.button = 0,
-//   });
-// }
+// Make sure these classes are defined (uncomment or add to your main_activity.dart)
